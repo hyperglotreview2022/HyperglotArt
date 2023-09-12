908fbb95661bc06e02a900dbf9e662ca59eb0289
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-09-12 13:52:39 +0530

    12

--- pages/makeRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export const makeRequest = axios.create({
-  baseURL:process.env.NEXT_PUBLIC_URL,
-  heaader:{
-    Authorization: "bearer "+process.env.NEXT_PUBLIC_TOKEN,
-  },
-})
