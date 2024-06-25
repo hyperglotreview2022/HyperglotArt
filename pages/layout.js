71100ae73d281eb7a3780607f154eb15114266d5
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-06-25 13:46:51 +0530

    251

--- pages/layout.js
+++ pages/layout.js
@@ -1,15 +1,15 @@
-"use client";
-import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
-import Auth from "./auth";
+// "use client";
+// import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
+// import Auth from "./auth";
 
-export default function RootLayout({ children }) {
-  return (
-    <KindeProvider>
-      <html lang="en">
-        <body>
-          <Auth>{children}</Auth>
-        </body>
-      </html>
-    </KindeProvider>
-  );
-}
+// export default function RootLayout({ children }) {
+//   return (
+//     <KindeProvider>
+//       <html lang="en">
+//         <body>
+//           <Auth>{children}</Auth>
+//         </body>
+//       </html>
+//     </KindeProvider>
+//   );
+// }
