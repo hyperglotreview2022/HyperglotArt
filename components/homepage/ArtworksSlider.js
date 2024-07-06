1a7cfdf680d0617a9263c37e457d045a0b9a0757
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-07-06 12:13:42 +0530

    061

--- components/homepage/ArtworksSlider.js
+++ components/homepage/ArtworksSlider.js
@@ -1,25 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import EventsSlider from "./EventsSlider";
 import styles from "../../styles/artworksslider.module.css";
+import Link from "next/link";
 
 export default function ArtworksSlider({ artworks }) {
-  const [offsetY, setOffsetY] = useState(1000);
-  const handleScroll = () => setOffsetY(window.pageYOffset);
-
-  useEffect(()=>{
-    window.addEventListener("scroll",handleScroll);
-  },[]);
 
   return (
     <div className={styles.container}>
       <div className={styles.topic}>Featured Artists</div>
       <div className={styles.flex}>
-        <div className={styles.item}>Show All</div>
-        <div className={styles.item}>Sculptures</div>
-        <div className={styles.item}>Best Curation</div>
-        <div className={styles.item}>Digital Art</div>
-        <div className={styles.item}>Marketplace Spaces</div>
-        <div className={styles.item}>Custom</div>
+        <Link className={styles.link} href="/Medium/paintings"><div className={styles.item}>Paintings</div></Link>
+        <Link  className={styles.link} href="/Medium/prints"><div className={styles.item}>Prints</div></Link>
+        <Link  className={styles.link} href="/Medium/sculptures"><div className={styles.item}>Sculptures</div></Link>
+        <Link  className={styles.link} href='/Genre/figurative'><div className={styles.item}>Figurative</div></Link>
+        <Link  className={styles.link} href='/Genre/abstract'><div className={styles.item}>Abstract</div></Link>
+        <Link  className={styles.link} href='/Genre/landscape'><div className={styles.item}>Landscape</div></Link>
       </div>
       <EventsSlider artworks={artworks}/>
       {/* <div className={styles.imgcontain} >
