3d877719af40733743baefb9b8c340fc75d6f406
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-07-10 16:33:57 +0530

    108

--- pages/api/events.js
+++ pages/api/events.js
@@ -7,7 +7,7 @@ const events = [
     info:{Date:"26th August, 2023", Venue:"CSOI Art Gallery, Chanakyaapuri, New Delhi", time:"10 am - 7 pm"},},
     { id:'2',
         name: 'INTERFACES - Part 2',
-        url:"/events/interfaces2.jpg",
+        url:"/events/interfaces2.JPG",
         url2:"/events/interfacespart21.png",
         text:[`Interfaces - Part 2 scheduled to be held from 28-31 July will showcase exquisite prints by Olivier Deprez and Roby Comblain but will also embrace other genres such as paintings, sculpture, and mixed media with a special focus on emerging artists and women artists. It will provide an opportunity for younger artists to share the spotlight and interact with renowned artists during the exhibition.`],
         info:{Date:"28th - 31st July, 2023", Venue:"Gallerie Romain Rolland , Alliance Française de Delhi , Lodhi Road, New Delhi", time:"11 am - 7 pm"},},
@@ -20,7 +20,7 @@ const events = [
     { id:'4',
         name: 'Art for Paws',
         url:"/events/artforpaws1.jpg",
-        url2:"/events/ArtforPaws12.jpg",
+        url2:"/events/ArtforPaws12.JPG",
         text:[`Hyperglot Review's first exhibition, Art for Paws, was a significant milestone in our mission to promote both established and emerging artists. We recognize that emerging artists often face barriers in gaining exposure and visibility, primarily due to economic and demographic constraints. With this exhibition, we aimed to provide coverage and support to these artists, fostering a diverse and inclusive artistic community.`,`Art for Paws showcased an exceptional collection of artworks by a talented group of Delhi-based artists. The featured artists included Shabana Qadri, Sweeta Rai, Subrata Ghosh, Pabitra Pal, Sheikh Hifzul, Shampa Bhattacharjee, Shayani Boral, Deb Dutta, Titas Das, Susnata Chatterjee, and Debangsu Biswas. This exhibition particularly emphasized promoting emerging artists, with a special focus on women artists, recognizing their unique perspectives and contributions to the art world.`,`In addition to supporting these talented artists, a portion of the proceeds from ticket sales was dedicated to feeding stray animals, reflecting our commitment to social responsibility and animal welfare.`,`The exhibition took place from 8th June to 11th June 2023. During these four days, visitors had the opportunity to experience the vibrant and diverse artworks created by these exceptional artists. Through this event, Hyperglot Review endeavored to provide a platform for emerging artists, celebrating their talents and contributing to the growth of the artistic community.`],
         info:{Date:"8th - 11th June, 2023", Venue:"Art Gallery, CSOI, Vinay Marg, Chanakyapuri, New Delhi", time:"11 am - 7 pm"},},
   ]
