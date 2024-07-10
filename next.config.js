a6ef74e0a1c1e317a909f6fd6993b5c23dba6303
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-07-10 13:42:26 +0530

    102

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['lh3.googleusercontent.com','gravatar.com'],
+    unoptimized: true
   },
   webpack: (config, { isServer }) => {
     // If client-side, don't polyfill `fs`
