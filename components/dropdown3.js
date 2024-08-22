9aad271b5412540513af428bb9c5b09f3f15f713
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-08-22 12:50:31 +0530

    822

--- components/dropdown3.js
+++ components/dropdown3.js
@@ -1,14 +1,31 @@
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {BsChevronDown} from 'react-icons/bs'
 import styles from '../styles/dropdown.module.css'
 
 const dropdown = ({colorChange}) => {
   const [dropd, setDropd] = useState(true)
+  const [width, setwidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setwidth(window.innerWidth);
+    };
+
+    // Add event listener to handle window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
 
   return (
     <div className={styles.container}>
-      <div onClick={()=>{setDropd(!dropd)}} className={styles.navlink}>Artists<BsChevronDown /></div>
+      <div 
+      onClick={()=>{width < 800 && setDropd(!dropd)}}
+      className={styles.navlink}>Artists<BsChevronDown /></div>
       <div className={dropd ? `${colorChange ? styles.content : styles.contentd}` : `${colorChange ? styles.content1 : styles.content1d}`}>
         <Link className={styles.link} href='/Artists/Featured'>Featured</Link>
         <Link className={styles.link} href='/Artists/Spotlight'>Spotlight</Link>
--- components/dropdownforpages.js
+++ components/dropdownforpages.js
@@ -1,14 +1,31 @@
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {BsChevronDown} from 'react-icons/bs'
 import styles from '../styles/dropdown.module.css'
 
 const dropdown = () => {
   const [dropd, setDropd] = useState(true)
+  const [width, setwidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setwidth(window.innerWidth);
+    };
+
+    // Add event listener to handle window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
 
   return (
     <div className={styles.container}>
-      <div onClick={()=>{setDropd(!dropd)}} className={styles.navlink}>Our Services<BsChevronDown /></div>
+      <div 
+      onClick={()=>{width < 800 && setDropd(!dropd)}}
+      className={styles.navlink}>Our Services<BsChevronDown /></div>
       <div className={dropd ? `${styles.contentd}` : `${styles.content1d}`}>
       <Link className={styles.link} href="/OurServices/Artists">Artists</Link>
         <Link className={styles.link} href='/OurServices/Buyers'>Buyers</Link>
