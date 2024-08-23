f42f2e16dce982b1f5c45b4fc4ca34932efc27f4
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-08-23 10:29:33 +0530

    44

--- components/homepage/herocomponent.js
+++ components/homepage/herocomponent.js
@@ -1,8 +1,24 @@
 import Image from "next/image"
 import styles from '../../styles/hero.module.css'
+import { useEffect, useState } from "react";
 
 
 const herocomponent = () => {
+  const [width, setwidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setwidth(window.innerWidth);
+    };
+
+    // Add event listener to handle window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
   return (
     <div className={styles.herosection}>
     <div className={styles.cover}>
@@ -11,7 +27,7 @@ const herocomponent = () => {
         <div className={styles.btm}>Bring home an exquisite work of art</div>
       </div>
     </div>
-      <video className={styles.video} src="/herovideo/herovideo.mp4" autoPlay loop muted preload="auto">
+      <video className={styles.video} src={width < 800 ? "/herovideo/mobvideo.mp4" : "/herovideo/herovideo.mp4"} autoPlay loop muted preload="auto">
       </video>
     </div>
   )
