3072162b6aed94c467e208981c1fb315a1c3b28d
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-08-23 12:55:00 +0530

    823

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
       <Head>
         <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
         <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
+        <link rel="icon" href="/ico.ico" />
+        <meta property="og:image" content="/ico.ico" />
+        <link rel="apple-touch-icon" href="/ico.ico"/>
       </Head>
       <body>
         <Main />
