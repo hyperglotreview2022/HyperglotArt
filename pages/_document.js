e38d826cf270b5226af55765f7963bf9e4cbc139
Pushkargaur10 <gaurpushkar10@gmail.com>
2024-10-04 11:42:07 +0530

    23

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="icon" href="/ico.ico" />
         <meta property="og:image" content="/ico.ico" />
         <link rel="apple-touch-icon" href="/ico.ico"/>
-        <title>Your Website Title</title>
+        <title>Hyperglot Art</title>
   <meta name="description" content="Hyperglot Solutions is a culmination of years of research and study to address a gap in the market to provide marketing and promotional services to artists who often are not able to get the required level of exposure and visibility due to certain economic and demographic constraints." />
   <meta name="keywords" content="hyperglot art, hyperglot, art, gallery, VR, AR, digital art" />
   <meta name="author" content="Hyperglot Art" />
